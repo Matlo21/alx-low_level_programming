4a73a6efc70ed8a20d65025c293ca7a928d19258
Matlo21 <mokomanematlotlo21@gmail.com>
2022-07-01 12:48:17 -0700

    My 10th commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - is the entry point 
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int number;
+
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+}
+
+if (number == 57)
+{
+break;
+}
+
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+
+}
