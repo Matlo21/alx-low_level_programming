ed74cd9e96d0173b12d269953243460b431dde7e
Matlo21 <mokomanematlotlo21@gmail.com>
2022-07-01 12:11:15 -0700

    My 7th commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - is the entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
