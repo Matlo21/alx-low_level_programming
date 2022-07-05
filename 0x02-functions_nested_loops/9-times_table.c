cd06589842e72bc1046dba788291aa039a4a03fc
Matlo21 <mokomanematlotlo21@gmail.com>
2022-07-05 09:07:26 -0700

    My updated 10th commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,10 +4,8 @@
  *
  * Return: Always 0.
  */
-
 void times_table(void)
 {
-
 int a;
 int b;
 int c;
@@ -33,26 +31,18 @@ if (b < 9)
 _putchar(',');
 _putchar (' ');
 }
-
 }
-
 else
 {
 _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
-
 if (b < 9)
 {
 _putchar(',');
 _putchar (' ');
 }
-
 }
-
 }
-
 _putchar ('\n');
-
 }
-
 }
