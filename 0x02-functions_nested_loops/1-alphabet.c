108129dde0302ea4fb9793e10f430da5b2943891
Matlo21 <mokomanematlotlo21@gmail.com>
2022-07-05 03:51:58 -0700

    Updated 2nd commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  *print_alphabet - print alphabet lowercase
  *
- *Return: Always 0 (Success)
+ *Return: Always 0.
  */
 
 void print_alphabet(void);
@@ -15,6 +15,6 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
 }
 
-_putchar(\n);
+_putchar('\n');
 
 }
