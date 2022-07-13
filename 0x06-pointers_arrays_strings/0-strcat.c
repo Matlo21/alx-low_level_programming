e91a237fb183fe887df2618dea5d9e99a02ebc81
Matlo21 <mokomanematlotlo21@gmail.com>
2022-07-13 10:57:02 -0700

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,6 +3,7 @@
  *_strcat - concatenates two strings
  *@dest: A pointer to a character that will be changed
  *@sric: A pointer to a character that will also be changed
+ *Return: dest
  */
 
 char *_strcat(char *dest, char *src)
